dfce2fb974ea82e303e661d59ab37998f0fefbba
Anthony Azubike Osigbe <45193993+anthonyosigbe@users.noreply.github.com>
2022-12-13 13:20:17 +0100

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,13 @@
+var cars = ['toyota', 'honda', 'mazda', 'mitsubishi', 'tesla', 'lexus'];
+
+cars [6] = 'mercedes-benz'
+console.log(cars[0]);
+var lastItem = cars[cars.length - 1];
+console.log(lastElement);
+
+for (var i=0; i < cars.length; i++){
+  console.log(cars[i]);
+
+}
+
+
